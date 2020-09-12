ab59db56b64b9902b8ef929b66b5a83281093dd0
Gustavo Leonato <gleonato@gmail.com>
2020-09-12 14:03:07 -0300

    1 image fix

--- GAN/MondriGAN.py
+++ GAN/MondriGAN.py
@@ -187,18 +187,8 @@ class GAN():
         # r, c = 5, 5
         noise = np.random.normal(0, 1, (1, self.latent_dim))
         gen_img = self.generator.predict(noise)
-        # print(gen_imgs.shape)
         gen_img = 0.5 * gen_img + 0.5
-        fig, axs = plt.subplots(1)
-        axs.imshow(gen_img)
-        # cnt = 0
-        # for i in range(r):
-        #     for j in range(c):
-        #         # axs[i, j].imshow(gen_imgs[cnt, :, :, 0], cmap='gray')
-        #         axs[i, j].imshow(gen_img)
-        #         axs[i, j].axis('off')
-        #         cnt += 1
-        fig.savefig("images/1-image-%d.png" % epoch)
+        plt.imshow(gen_img[0, :, :, 0]).figure.savefig("images/1-image-%d.png" % epoch)
         plt.close()
 
 
